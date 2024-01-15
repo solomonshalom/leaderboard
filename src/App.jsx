9c509f8eeb3c6ce20a08423a82d4a8b7b30cfc6a
Solomon Shalom Lijo <solomonlijo@skiff.com>
2024-01-15 22:41:31 +0530

    Hello! Mom

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./View/Home";
 import { Toaster } from "react-hot-toast";
 import Board from "./View/Board";
-import About from "./View/About";
 
 export default function App() {
   return (
@@ -10,7 +9,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/board/:slug" element={<Board />} />
-        <Route path="/about" element={<About />} />
       </Routes>
       <Toaster />
     </BrowserRouter>
